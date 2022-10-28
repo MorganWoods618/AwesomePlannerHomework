3d694b69396cc19241cd59e16dbe3acce92a459b
Morgan Woods <morgan4242564@gmail.com>
2022-10-27 23:03:25 -0600

    saving data

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+//local storage hour buttons
 $("#hour-9 .description").val(localStorage.getItem("hour-9"))
 $("#hour-10 .description").val(localStorage.getItem("hour-10"))
 $("#hour-11 .description").val(localStorage.getItem("hour-11"))
@@ -11,7 +12,7 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"))
 $("#hour-16 .description").val(localStorage.getItem("hour-16"))
 $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
-//local storage
+//local storageon click event
 $(".saveBtn").on("click", function () {
     var textvalue = $(this).siblings(".description").val()
     var hour = $(this).parent().attr("id")
@@ -19,36 +20,9 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(hour, textvalue)
 })
 
+//Clear Local Storage
 $("#clear-search").on("click", function () {
     localStorage.clear();
     location.reload();
 }
 )
-
-//color coding
-
-function colorCode() {
-    var momentTime = moment().hours()
-    $(".time-block").each(function () {
-        var currentTime = parseInt($(this).attr("id").split("-")[1]);
-
-        if (currentTime < momentTime) {
-            // $(this).removeClass("present future")
-            $(this).addClass("past")
-        }
-
-        else if (currentTime == momentTime) {
-            $(this).removeClass("past, future")
-            $(this).addClass("present")
-        }
-
-        else {
-            $(this).removeClass("past, present")
-            $(this).addClass("future")
-        }
-
-    })
-
-}
-
-colorCode()
